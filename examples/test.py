7e80e85f02b9507d42c3622efe6ee03d1f933be8
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2025-03-18 00:21:31 +0200

    Plot measured voltages and deviations.

--- examples/test.py
+++ examples/test.py
@@ -7,6 +7,7 @@
 
 import serial
 import time
+import matplotlib.pyplot as plt
 
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=10)
 
@@ -19,19 +20,44 @@ print(ser.readline())
 refs = {b'VCC' : 3, b'INT1' : 1, b'INT2' : 2, b'INT4' : 4}
 
 for ref in refs:
+	xx = []
+	yv = []
+	yd = []
 	ser.write(b":CONF:ANAL:VREF %s\n" % ref)
 	ser.flush()
 	time.sleep(1)
-	for v in [0.1 * refs[ref], 0.25 * refs[ref], 0.5 * refs[ref], 0.8 * refs[ref]]:
+
+	for i in range(1, 11):
+		v = i * 0.1 * refs[ref]
+		vals = []
+		devs = []
 		ser.write(b":SOUR:VOLT %f\n" % v)
 		ser.flush()
+		time.sleep(1)
+
 		for i in range(0, 10):
-			time.sleep(1)
 			ser.write(b":MEAS:VOLT? 1\n")
 			ser.flush()
 			val = float(ser.readline())
 			dev = (val - v) * 100 / v
-			print("Ref = " + str(ref) + ": " + str(val) + "V; deviation " + str(dev) + "%")
+			vals.append(val)
+			devs.append(dev)
+			time.sleep(0.1)
+
+		val = sum(vals) / len(vals)
+		dev = sum(devs) / len(devs)
+		print("Ref = " + str(ref) + ": Uo = " + str(v)  + "V -> Ui = " + str(val) + "V; deviation = " + str(dev) + "%")
+
+		xx.append(v)
+		yv.append(val)
+		yd.append(dev)
+
+	fig, (av, ad) = plt.subplots(2, 1)
+	av.plot(xx, yv)
+	ad.plot(xx, yd)
+	av.grid(True)
+	ad.grid(True)
+	plt.show()
 
 #	for i in range(0, 256):
 #		ser.write(b":SOUR:VOLT %d\n" % i)
